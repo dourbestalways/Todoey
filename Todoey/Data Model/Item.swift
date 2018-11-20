e927033ca87d34c126c23a1d2bca64553fe82bb2
Rax Kim <dourbestalways@gmail.com>
2018-11-20 17:07:28 +0900

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Sung Hwan Kim on 20/11/2018.
+//  Copyright © 2018 Sung Hwan Kim. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title : String = ""
+    var done : Bool = false
+    
+}
