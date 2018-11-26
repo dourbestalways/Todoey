c73143bedbeb55722c0b9c7c89af24c03321f66e
Rax Kim <dourbestalways@gmail.com>
2018-11-26 12:37:42 +0900

    add another tableView to categorize items.

---
+++ Todoey/Controllers/CategoryViewController.swift
@@ -0,0 +1,36 @@
+//
+//  CategoryViewController.swift
+//  Todoey
+//
+//  Created by Sung Hwan Kim on 26/11/2018.
+//  Copyright © 2018 Sung Hwan Kim. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class CategoryViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    //MARK: - TableView Datasource Methods
+    
+    
+    //MARK: - TableView Delegate Methods
+    
+    
+    //MARK: - Add New Categories
+    
+
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+    }
+    
+    
+    //MARK: - Data Manipulation Methods
+    
+}
